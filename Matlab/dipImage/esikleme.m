0fd451d565187234951bb20fae5b36f6c2f49d1c
Didem Yanardağ <didem.yanardag@bil.omu.edu.tr>
2015-03-11 00:14:57 +0200

    Update esikleme.m

--- Matlab/dipImage/esikleme.m
+++ Matlab/dipImage/esikleme.m
@@ -1,8 +1,22 @@
 clc
 clear all
-imge = imread('cameraman.tif');
-imge2 = im2bw(imge,0.5);
-imshow(imge2),title('eşiklenmiş görüntü')
-Mop = imge2<0.5;
-Map = imge2>0.5;
-T = (Mop+Map)/2;
+
+I = imread('cameraman.tif');
+bw = im2bw(I);
+subplot(1,3,1); imshow(I);
+subplot(1,3,2); imshow(bw);
+
+%%%%%tresholding%%%%
+T = 0.5;
+[x,y] = size(bw);
+for i = 1:x
+    for j = 1:y
+        if bw(x,y) >= T
+            Mop= Mop + 1;
+        else
+            Map = Map + 1;
+        end
+    end
+end
+figure();
+T2 = (Mop + Map) / 2;
