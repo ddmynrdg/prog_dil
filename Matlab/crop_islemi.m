b606a9e53151743f089492c7be23c9b3983d66e5
Didem Yanardağ <didem.yanardag@bil.omu.edu.tr>
2015-03-27 12:07:16 +0200

    WriteBalance_ilkasama

---
+++ Matlab/crop_islemi.m
@@ -0,0 +1,129 @@
+clear all;
+clc;
+close all;
+% AÇIKLAMA:Veri tabanına yüklenen bütün resimler MATLAB klasöründe "resimmler" adlı bir 
+% klasörde toplanıldı.Teker teker bu resimler çağrıldı. Renk paletleri
+% resinde bulunduğu pixel aralına göre crop edildi.
+ 
+% 10 isimli resim ve renk paletini crop edilmiş hali
+ im_10 = imread('resimmler/10.JPG');
+ im2_10 = imcrop(im_10, [610 117 1095 780]);
+ imshow(im_10),figure,imshow(im2_10)
+ 
+% 10a isimli resim ve renk paletini crop edilmiş hali
+ im_10a = imread('resimmler/10a.JPG');
+ im2_10a = imcrop(im_10a, [670 117 1005 720]);
+ imshow(im_10a),figure,imshow(im2_10a)
+ 
+% 10b isimli resim ve renk paletini crop edilmiş hali
+ im_10b = imread('resimmler/10b.JPG');
+ im2_10b = imcrop(im_10b, [440 147 1100 800]);
+ imshow(im_10b),figure,imshow(im2_10b)
+ 
+ 
+% 10c isimli resim ve renk paletini crop edilmiş hali
+ im_10c = imread('resimmler/10c.JPG');
+ im2_10c = imcrop(im_10c, [590 260 1100 800]);
+ imshow(im_10c),figure,imshow(im2_10c)
+ 
+ 
+% 11 isimli resim ve renk paletini crop edilmiş hali
+ im_11 = imread('resimmler/11.JPG');
+ im2_11 = imcrop(im_11, [470 117 1205 820]);
+ imshow(im_11),figure,imshow(im2_11)
+ 
+ 
+% 11A isimli resim ve renk paletini crop edilmiş hali
+ im_11A = imread('resimmler/11A.JPG');
+ im2_11A = imcrop(im_11A, [500 150 1205 820]);
+ imshow(im_11A),figure,imshow(im2_11A)
+ 
+% 11B isimli resim ve renk paletini crop edilmiş hali
+ im_11B = imread('resimmler/11B.JPG');
+ im2_11B = imcrop(im_11B, [540 140 1150 800]);
+ imshow(im_11B),figure,imshow(im2_11B)
+ 
+ 
+% 11C isimli resim ve renk paletini crop edilmiş hali
+ im_11C = imread('resimmler/11C.JPG');
+ im2_11C = imcrop(im_11C, [480 270 1160 800]);
+ imshow(im_11C),figure,imshow(im2_11C)
+ 
+% 12 isimli resim ve renk paletini crop edilmiş hali
+ im_12 = imread('resimmler/12.JPG');
+ im2_12 = imcrop(im_12, [670 450 1005 700]);
+ imshow(im_12),figure,imshow(im2_12)
+ 
+% 12a isimli resim ve renk paletini crop edilmiş hali
+ im_12a = imread('resimmler/12a.JPG');
+ im2_12a = imcrop(im_12a, [650 460 1000 750]);
+ imshow(im_12a),figure,imshow(im2_12a)
+ 
+% 12b isimli resim ve renk paletini crop edilmiş hali
+ im_12b = imread('resimmler/12b.JPG');
+ im2_12b = imcrop(im_12b, [700 550 1005 720]);
+ imshow(im_12b),figure,imshow(im2_12b)
+ 
+% 12c isimli resim ve renk paletini crop edilmiş hali
+im_12c = imread('resimmler/12c.JPG');
+im2_12c = imcrop(im_12c, [590 300 1105 800]);
+imshow(im_12c),figure,imshow(im2_12c)
+ 
+ 
+% 13 isimli resim ve renk paletini crop edilmiş hali
+ im_13 = imread('resimmler/13.JPG');
+ im2_13 = imcrop(im_13, [750 260 1000 700]);
+ imshow(im_13),figure,imshow(im2_13)
+ 
+ 
+% 13 isimli resim ve renk paletini crop edilmiş hali
+ im_13a = imread('resimmler/13a.JPG');
+ im2_13a = imcrop(im_13a, [700 200 1005 700]);
+ imshow(im_13a),figure,imshow(im2_13a)
+ 
+% 13b isimli resim ve renk paletini crop edilmiş hali
+ im_13b = imread('resimmler/13b.JPG');
+ im2_13b = imcrop(im_13b, [750 190 1000 670]);
+ imshow(im_13b),figure,imshow(im2_13b)
+ 
+% 14 isimli resim ve renk paletini crop edilmiş hali
+ im_14 = imread('resimmler/14.JPG');
+ im2_14 = imcrop(im_14, [490 280 1000 670]);
+ imshow(im_14),figure,imshow(im2_14)
+ 
+% 14a isimli resim ve renk paletini crop edilmiş hali
+ im_14a = imread('resimmler/14a.JPG');
+ im2_14a = imcrop(im_14a, [550 190 980 650]);
+ imshow(im_14a),figure,imshow(im2_14a)
+ 
+% 14b isimli resim ve renk paletini crop edilmiş hali
+ im_14b = imread('resimmler/14b.JPG');
+ im2_14b = imcrop(im_14b, [600 190 1000 670]);
+ imshow(im_14b),figure,imshow(im2_14b)
+ 
+% 14c isimli resim ve renk paletini crop edilmiş hali
+ im_14c = imread('resimmler/14c.JPG');
+ im2_14c = imcrop(im_14c, [520 300 1000 650]);
+ imshow(im_14c),figure,imshow(im2_14c)
+ 
+% 15 isimli resim ve renk paletini crop edilmiş hali
+ im_15 = imread('resimmler/15.JPG');
+ im2_15 = imcrop(im_15, [790 400 900 600]);
+ imshow(im_15),figure,imshow(im2_15)
+ 
+% 15a isimli resim ve renk paletini crop edilmiş hali
+ im_15a = imread('resimmler/15a.JPG');
+ im2_15a = imcrop(im_15a, [650 300 880 600]);
+ imshow(im_15a),figure,imshow(im2_15a)
+ 
+% 15b isimli resim ve renk paletini crop edilmiş hali
+im_15b = imread('resimmler/15b.JPG');
+im2_15b = imcrop(im_15b, [670 400 900 600]);
+imshow(im_15b),figure,imshow(im2_15b)
+ 
+ 
+% 15c isimli resim ve renk paletini crop edilmiş hali
+im_15c = imread('resimmler/15c.JPG');
+im2_15c = imcrop(im_15c, [880 350 900 600]);
+imshow(im_15c),figure,imshow(im2_15c)
+ 
