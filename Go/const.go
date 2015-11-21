cca243c8b2b2f84903a5c0a57e11d2b9fad8a04d
Didem Yanardağ <didem.yanardag@bil.omu.edu.tr>
2015-11-21 20:29:24 +0200

    Exercises

---
+++ Go/const.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+import "math"
+
+const str string = "const"
+
+func main() {
+	fmt.Println(str)
+	
+	const s1 = 6000000000
+	
+	const s2 = 3e20/n
+	fmt.Println(int64(s1))
+	
+	fmt.Println(math.Sin(s2))
+}
